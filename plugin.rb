92097b4a3e3028e25c437425033acb4a8a3e3497
familycfo <41447664+familycfo@users.noreply.github.com>
2019-02-18 03:30:03 -0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -18,7 +18,7 @@ module ::Blog
   end
 end
 
-Rails.configuration.assets.precompile += ['LAB.js', 'blog.css']
+
 
 after_initialize do
 
@@ -39,48 +39,4 @@ after_initialize do
     end
   end
 
-  load File.expand_path("../app/jobs/blog_update_twitter.rb", __FILE__)
-  load File.expand_path("../app/jobs/blog_update_stackoverflow.rb", __FILE__)
-
-  require_dependency "plugin/filter"
-
-  Plugin::Filter.register(:after_post_cook) do |post, cooked|
-    if post.post_number == 1 && post.topic && post.topic.archetype == "regular"
-      split = cooked.split(/<hr\/?>/)
-
-      if split.length > 1
-        post.topic.custom_fields["summary"] = split[0]
-        post.topic.save unless post.topic.new_record?
-        cooked = split[1..-1].join("<hr>")
-      end
-    end
-    cooked
-  end
-
-  class BlogConstraint
-    def matches?(request)
-      request.host == BLOG_HOST
-    end
-  end
-
-  class ::Topic
-    before_save :blog_bake_summary
-    before_save :ensure_permalink
-
-    def ensure_permalink
-      unless custom_fields["permalink"]
-        custom_fields["permalink"] =  (Time.now.strftime "/archive/%Y/%m/%d/") + self.slug
-      end
-    end
-
-    def blog_bake_summary
-      if summary = custom_fields["summary"]
-        custom_fields["cooked_summary"] = PrettyText.cook(summary)
-      end
-    end
-  end
-
-  Discourse::Application.routes.prepend do
-    mount ::Blog::Engine, at: "/", constraints: BlogConstraint.new
-  end
-end
+  
